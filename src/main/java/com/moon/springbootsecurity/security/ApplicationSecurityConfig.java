b5a16c8c07d31dca0b4a92ca4c98a1799932af9f
dilbaay23 <dilbaay@gmail.com>
2020-12-11 13:54:19 +0100

    Authentication Provider

--- src/main/java/com/moon/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/moon/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -4,18 +4,17 @@ package com.moon.springbootsecurity.security;
  * Created by Moon on 12/11/2020
  */
 
+import com.moon.springbootsecurity.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
@@ -29,10 +28,12 @@ import static com.moon.springbootsecurity.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService  applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     // we choose this method to secure things
@@ -57,7 +58,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .rememberMe()                    // default to 2 weeks
                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
                 .key("somethingverysecured")
-                .userDetailsService(userDetailsServiceBean())
+                .userDetailsService(applicationUserService)
 
                 .and()
                 .logout()
@@ -72,29 +73,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
     @Bean
-    public UserDetailsService userDetailsServiceBean() throws Exception {      // this is for how you retrieve your users from the database
-        UserDetails moonUser = User.builder()
-                .username("moon")
-                .password(passwordEncoder.encode("password"))
-                //              .roles(ApplicationUserRole.STUDENT.name())  //ROLE_STUDENT
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-        UserDetails adminUser = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("password"))
-                //               .roles(ApplicationUserRole.ADMIN.name())  //ROLE_ADMIN
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails coUser = User.builder()
-                .username("co")
-                .password(passwordEncoder.encode("password"))
-                //              .roles(ApplicationUserRole.ADMINTRAINEE.name())  //ROLE_ADMINTRAINEE
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(moonUser, adminUser, coUser);    //this is a class (InMemoryUserDetailsManager) which implements UserDetailsService
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
     }
 }
